f15bab2883320cc5f213f0d5b9f6ed5fe62dc91c
Harshit7929 <147587880+Harshit7929@users.noreply.github.com>
2025-11-20 18:20:45 +0530

    Configure base URL and optimize dependencies
    Set base URL for GitHub Pages and optimize dependencies.

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/text_highlighter/",   // <--- important for GitHub Pages repo URL
   plugins: [react()],
-  server: {
-    port: 5173
+  optimizeDeps: {
+    include: ["pdfjs-dist/legacy/build/pdf"]
   }
-})
+});
+
