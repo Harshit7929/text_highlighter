927f574e0640f13b7a44cb1f7d01f3d7fdbdd5ed
gulipalliharshit@gmail.com <gulipalliharshit@gmail.com>
2025-11-20 22:46:01 +0530

    fix: correct vite base and repoName

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const repoName = 'text_highlighter'   // <-- correct name (with g)
+const repoName = 'text_highlighter' 
 
 export default defineConfig({
   plugins: [react()],
